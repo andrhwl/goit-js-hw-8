18be7df48cb953fe9481be76815a313cd57edcb6
andrhwl <andrhwl@gmail.com>
2023-05-20 19:40:50 +0300

    addd

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,42 +1,31 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-const emailInput = document.querySelector('input[name=email]');
-const messageInput = document.querySelector('textarea[name=message]');
-const btnSubmit = document.querySelector('button[type=submit]');
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+populateForm();
+form.addEventListener('input', throttle(onMessageInput, 500));
+form.addEventListener('submit', onFormSubmit);
 
+function onMessageInput() {
+  const objectSave = { email: email.value, message: message.value };
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(objectSave));
+}
 
-const saveLocalStorage =
-  throttle(() => {
-  const formState = {
-    email: emailInput.value,
-    message: messageInput.value,
-  };
-  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
-}, 500);
-
-form.addEventListener('input', saveLocalStorage);
-
-window.addEventListener('DOMContentLoaded', () => {
-  const formState = JSON.parse(localStorage.getItem('feedback-form-state'))
-  if (formState)
-    emailInput.value = formState.email || '',
-    messageInput.value = formState.message || ''
-});
-
-function clearData(evt){
-  evt.preventDefault();
-  const clearDataStorage = localStorage.getItem(LOCAL_KEY);
-  const clearDataStorageJSON = JSON.parse(clearDataStorage);
-  if(areaMsg.value ==="" || inputEmail.value ===""){
-      alert("Input required!");
-     return;
-  }else{
-     console.log(clearDataStorageJSON);
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (email.value === '' || message.value === '') {
+    return alert('Заповніть будь-ласка, всі поля!');
   }
- 
-  localStorage.removeItem(LOCAL_KEY);
-  form.reset();
-  delete dataStorage.email;
-  delete dataStorage.message;
-};
+  console.log({ email: email.value, message: message.value });
+  event.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+function populateForm() {
+  const savedObject = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  if (savedObject) {
+    email.value = savedObject.email;
+    message.value = savedObject.message;
+  }
+}
